68ebde19697c14f7c85e9cbba3a17ba23190301f
FBB-Fadel <fadelbamba1306@gmail.com>
2026-09-15 15:35:37 +0200

    exercice 1

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿decimal solde = 1000m;
+decimal montant = 150m;
+
+if(montant > 0)
+{
+    solde += montant;
+}
+else if (montant < 0)
+{
+    decimal débit = -montant;
+    if(débit <= solde)
+    {
+        solde -= débit;
+    }
+    else
+    {
+        Console.WriteLine("solde Insuffisant");
+    }
+}
+Console.WriteLine($"Solde: {solde}");
+
+public decimal Crediter(decimal montant, decimal solde)
+{
+    if(montant >0)
+}
+{
+    
+}
+
